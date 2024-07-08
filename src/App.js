edc66603dd0d1150612db1db30dc982e4a339f5a
mbougs <nkanamario@yahoo.com>
2024-07-08 15:52:19 +0100

    from function to class

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    );
-    return (
-        <div>
-            Hello World!
-        </div>
-    )
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (error) => console.log(error)
+//     );
+//     return (
+//         <div>
+//             Latitude: 
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+        return(
+            <div>
+                You are in the Northern Hemisphere
+            </div>
+        )
+    }
 }
 
 export default App;
