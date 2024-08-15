f9d0286064ff48778b8eb65c53be73f33c7126fe
mbougs <nkanamario@yahoo.com>
2024-08-15 20:00:44 +0100

    project complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HemisphereDisplay from './HemisphereDisplay';
 
-// const App = () => {
-//     window.navigator.geolocation.getCurrentPosition(
-//         (position) => console.log(position),
-//         (error) => console.log(error)
-//     );
-//     return (
-//         <div>
-//             Latitude: 
-//         </div>
-//     )
-// }
+const App = () => {
+    const [ latitude, setLatitude ] = useState(null);
+    // const [ longitude, setLongitude ] = useState(null);
+    const [ errorMessage, setErrorMessage ] = useState('');
 
-class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (error) => console.log(error)
-        );
-        return(
-            <div>
-                You are in the Northern Hemisphere
-            </div>
-        )
-    }
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+            setLatitude( position.coords.latitude )
+            // setLongitude( position.coords.longitude )
+        },
+        (error) => {
+            setErrorMessage( error.message )
+        }
+    );
+    return (
+        <>
+        { errorMessage && !latitude && <div>{ errorMessage }</div> }
+        { !errorMessage && latitude && <div><HemisphereDisplay latitude={ latitude } /></div> }
+        {/* { !errorMessage && (latitude && longitude) && <div><HemisphereDisplay latitude={ latitude } longitude={ longitude } /></div> } */}
+        { !errorMessage && !latitude && <div>Loading...</div> }
+        </>
+    )
 }
 
 export default App;
+
+// class App extends React.Component {
+//     constructor (props) {
+//         super(props)
+        
+//         this.state = { latitude: null, longitude: null, errorMessage: '' }
+//         window.navigator.geolocation.getCurrentPosition(
+//             (position) => {
+//                 this.setState({ latitude: position.coords.latitude })
+//                 this.setState({ latitude: position.coords.longitude })
+//             },
+//             (error) => {
+//                 this.setState({ errorMessage: error.message })
+//             }
+//         );
+//     }
+//     render() {
+//         return(
+//             <div>
+//                 <p>
+//                 { this.state.latitude }
+//                 </p>
+//                 <p>
+//                 { this.state.longitude }
+//                 </p>
+//                 <p>
+//                 { this.state.errorMessage }
+//                 </p>
+//             </div>
+//         )
+//     }
+// }
+
+// export default App;
--- src/HemisphereDisplay.js
+++ src/HemisphereDisplay.js
@@ -1,9 +1,33 @@
 import React from "react";
+import './Hemisphere.css';
+import northernPic from './images/NorthernHemisphere.jpg'
+import southernPic from './images/SouthernHemisphere.jpg'
 
-const HemisphereDisplay = () => {
+const hemisphereConfig = {
+    Northern: {
+        text: "You are in the Northen Hemisphere",
+        picture: northernPic
+    },
+    Southern: {
+        text: "You are in the Southern Hemisphere",
+        picture: southernPic
+    }
+};
+
+const HemisphereDisplay = ({ latitude }) => {
+    const hemisphere = latitude > 0? "Northern" : "Southern";
+    const { text, picture } = hemisphereConfig[hemisphere];
+
     return (
-        <div>
-            Hey you are in Nothern hemisphere!
+        <div className={hemisphere}>
+            <div className="ui raised text container segment">
+            <div className="image">
+                <img src={picture} alt="north-south-hemisphere" />
+            </div>
+            <div className="ui teal bottom attached label">
+                <h1>{ text }</h1>
+            </div>
+            </div>
         </div>
     )
 }
