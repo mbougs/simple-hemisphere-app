363a66839a1abb93918ac0327fd1a6044cf308a1
mbougs <nkanamario@yahoo.com>
2024-08-16 23:08:45 +0100

    added vercel speed insight package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
   <App />
+  <SpeedInsights />
+  </>
 );
